41b656a00ca62d86b65f4a1412ee5465ae1008b1
sureshsumanimg <sureshkr.img@gmail.com>
2022-09-22 11:19:48 +0530

    guest modal create

--- resources/views/new-design/dashboard.blade.php
+++ resources/views/new-design/dashboard.blade.php
@@ -54,10 +54,88 @@
             </div>
         </div>
         <div class="col-xl-6 col-sm-4">
-            <div class="text-end"><button class="shadow-none border-0 text-white bg-theme1 rounded fs-14 fw-500 px-4 py-2">CREATE GUEST</button></div>
+            <div class="text-end"><button class="shadow-none border-0 text-white bg-theme1 rounded fs-14 fw-500 px-4 py-2" data-bs-toggle="modal" data-bs-target="#exampleModal">CREATE GUEST</button></div>
         </div>
         <div class="col-12 mt-4">
             <!-- Data Table -->
+        </div>
+    </div>
+</div>
+
+<!-- Modal Of Create Guest -->
+
+<!-- Button trigger modal -->
+<!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button> -->
+
+<!-- Modal -->
+<div class="modal fade Create-Guest-modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header py-3 border-0">
+                <h5 class="modal-title fs-20 text-theme2 fw-bold" id="exampleModalLabel">Edit Guest Information</h5>
+                <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+                <i class="imgr img-times fs-20" data-bs-dismiss="modal" aria-label="Close"></i>
+            </div>
+            <div class="modal-body p-0">
+                <div class="row px-0 mx-0">
+                    <div class="col-12 Account event_info px-0 py-2">
+                        <h4 class="m-0 heading fw-bold text-white px-3 fs-16">Guest Information</h4>
+                    </div>
+                    <div class="col-12 my-3">
+                        <div class="row">
+                            <div class="col-3">
+                                <!-- userName -->
+                                <div class="h-80px w-80px rounded-circle d-flex justify-content-center align-items-center bg-dark"><span class="fs-30 text-white fw-bold">DW</span></div>
+                                <!-- user-Name End -->
+
+                                <!-- user_img -->
+                                <div class="d-none">
+                                    <input class="form-control d-none" type="file" id="chooseFile">
+                                    <label for="chooseFile" class="h-80px w-80px rounded-circle d-flex justify-content-center align-items-center bg-dark text-white"><img src="{{ asset('/public/new-design/img/USER.jpg') }}" alt="" class="w-100 rounded-circle"></label>
+                                </div>
+                                <!-- user-img End -->
+                            </div>
+
+                            <div class="col-9">
+                                <form action="" class="modal-form">
+                                    <!-- <div class="col-12 input-group mb-3">
+                                        <select class="form-select shadow-none Inpt border-0" id="inputGroupSelect01">
+                                            <option class="choose py-1" selected="">Select Timezone</option>
+                                            <option class="choose py-1" value="1">One</option>
+                                            <option class="choose py-1" value="2">Two</option>
+                                            <option class="choose py-1" value="3">Three</option>
+                                        </select>
+                                    </div> -->
+                                    <div class="col-12 form-floating">
+                                        <select class="form-select shadow-none Inpt border-0" id="floatingSelect" aria-label="Floating label select example">
+                                            <option lass="choose py-1" selected>Open this select menu</option>
+                                            <option lass="choose py-1" value="1">One</option>
+                                            <option lass="choose py-1" value="2">Two</option>
+                                            <option lass="choose py-1" value="3">Three</option>
+                                        </select>
+                                        <label for="floatingSelect" class="fs-12">Status</label>
+                                    </div>
+                                    <div class="col-3 form-floating">
+                                        <select class="form-select shadow-none Inpt border-0" id="floatingSelect" aria-label="Floating label select example">
+                                            <option lass="choose py-1" selected>Open this select menu</option>
+                                            <option lass="choose py-1" value="1">One</option>
+                                            <option lass="choose py-1" value="2">Two</option>
+                                            <option lass="choose py-1" value="3">Three</option>
+                                        </select>
+                                        <label for="floatingSelect" class="fs-12">Status</label>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
         </div>
     </div>
 </div>
