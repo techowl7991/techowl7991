74435719e5083b9a048a5196024dd4179453da69
sureshsumanimg <sureshkr.img@gmail.com>
2022-09-23 15:26:59 +0530

    data table checkbox add

--- resources/views/new-design/dashboard.blade.php
+++ resources/views/new-design/dashboard.blade.php
@@ -276,8 +276,19 @@
 </script>
 <script>
     $(document).ready(function() {
-        $('#example').DataTable();
-    });
+    $('#example').DataTable( {
+        columnDefs: [ {
+            orderable: false,
+            className: 'select-checkbox',
+            targets:   0
+        } ],
+        select: {
+            style:    'os',
+            selector: 'td:first-child'
+        },
+        order: [[ 1, 'asc' ]]
+    } );
+} );
 </script>
 
 <script>
