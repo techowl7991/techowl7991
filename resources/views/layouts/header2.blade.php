4159a5784dee47368e3fa43a578ddce3965e5a2b
Aachuki <aachuki.img@gmail.com>
2022-09-27 18:27:45 +0530

    apply condition for header

--- resources/views/layouts/header2.blade.php
+++ resources/views/layouts/header2.blade.php
@@ -3,25 +3,28 @@
         <a class="navbar-brand w-sm-60px w-45px py-0" href="#"><img
                 src="{{ asset('/public/new-design/img/logo-1.png') }}" alt="" class="w-100"></a>
 
-
-        {{-- @if ('campaign_information' == request()->path()) --}}
-            {{-- <div class="nav-body d-flex align-items-center gap-3" id="navbarSupportedContent">
-                <a href=""
-                    class="shadow-none border-0 text-decoration-none text-theme2 bg-light fs-14 fw-bold px-sm-3 p-1 px-2 py-1 top_right_export_btn text-uppercase rounded-5">Back</a>
-                <a href=""
-                    class="shadow-none border-0 text-decoration-none text-white bg-theme1 fs-14 fw-bold px-sm-3 p-1 px-2 py-1 top_right_export_btn text-uppercase rounded-5">Save
-                    Changes</a>
-            </div> --}}
-        {{-- @else --}}
-            <!-- Right Side Start -->
-            <div class="nav-body d-flex align-items-center" id="navbarSupportedContent">
-                <!-- SideClick Button -->
+        <!-- Right Side Start -->
+        <div class="nav-body d-flex align-items-center" id="navbarSupportedContent">
+            <!-- SideClick Button -->
+            @if (request()->is('campaign_information'))
                 <div class="me-3">
                     <button id="SideClick"
-                        class="show_sidebar_btn shadow-none border-0 rounded bg-theme2 d-flex justify-content-center align-items-center"><i
-                            class="imgs img-bars fs-sm-25 fs-20 text-white"></i></button>
+                        class="show_sidebar_btn shadow-none border-0 rounded bg-theme2 d-flex  justify-content-center align-items-center d-none"><i
+                            class="imgs img-bars fs-sm-25 fs-20 text-white"></i>
+                    </button>
                 </div>
-                <!-- SideClick Button End-->
+                <a href="#"
+                    class="shadow-none border-0 text-decoration-none text-theme2 bg-light fs-14 fw-bold px-sm-3 p-1 px-2 py-1 top_right_export_btn text-uppercase rounded-5">Back</a>
+                <a href="#"
+                    class="shadow-none border-0 text-decoration-none text-white bg-theme1 fs-14 fw-bold px-sm-3 p-1 px-2 py-1 top_right_export_btn text-uppercase rounded-5 ms-3">Save
+                    Changes</a>
+            @else
+              <div class="me-3">
+                <button id="SideClick"
+                    class="show_sidebar_btn shadow-none border-0 rounded bg-theme2 d-flex  justify-content-center align-items-center"><i
+                        class="imgs img-bars fs-sm-25 fs-20 text-white"></i>
+                </button>
+            </div>
                 <ul class="navbar-nav me-auto mb-0">
                     <li class="nav-item dropdown">
                         <a class="nav-link h-sm-35px h-30px w-sm-35px w-30px  rounded-circle bg-white d-flex justify-content-center align-items-center ms-auto"
@@ -42,8 +45,8 @@
                         </ul>
                     </li>
                 </ul>
-            </div>
-            <!-- Right Side End -->
-        {{-- @endif --}}
+                <!-- Right Side End -->
+        </div>
+        @endif
     </div>
 </nav>
